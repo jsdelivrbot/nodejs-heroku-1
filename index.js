4f171528cd201d5e71ac580d313151735adda3c7
killvung <kiulam125@gmail.com>
2017-05-28 14:41:36 -0500

    Extract data more

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/webhook/', function (req, res) {
 
 app.post('/webhook/', function (req, res) {
     console.log("messaging_events")
-    console.log(req.body);    	
+    console.log(req.body.entry.changes);    	
     res.sendStatus(200)    
 })
 
